57bf0688c60e5052b0c464ef35368828fb5af965
randyluna222 <108312411+randyluna222@users.noreply.github.com>
2023-06-27 20:04:55 -0600

    Error del link
    Solución del error del link en el app.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-function copyFile(origen, destino) {
-  fs.readFile ('origen.txt' , origen, 'utf8', (error, data) => {
+function copyFile(origin, destiny) {
+  fs.readFile ( origin, 'utf8', (error, data) => {
     if (error) {
       console.error('No se puede leer el archivo origen.txt:', error);
       return;
     }
 
-    fs.writeFile('destino.txt', destino, data, 'utf8', (error) => {
+    fs.writeFile(destiny, data, 'utf8', (error) => {
       if (error) {
         console.error('Error al escribir en el archivo de destino:', error);
         return;
@@ -19,4 +19,3 @@ function copyFile(origen, destino) {
 }
 
 module.exports = copyFile
-  
